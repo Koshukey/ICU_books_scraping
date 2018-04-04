23cae0746ff296ee3d6392710ba5595fd31ab624
Koshukey <kosukeyoshimura@yoshimuramiyukitsubasanoMacBook.local>
2018-04-04 20:47:06 +0900

    ファイルにも検索結果を出力させるようにした

--- scraping_with_number.rb
+++ scraping_with_number.rb
@@ -3,8 +3,8 @@ require 'pry'
 require 'mechanize'
 
 def search_books(search_word)
-  puts "-------検索でヒットした本一覧-------"
 
+  puts "-------検索でヒットした本一覧-------"
   driver = Selenium::WebDriver.for :chrome #ドライバ選択
   driver.navigate.to "https://opac.icu.ac.jp/" #移動サイト指定
   search_form = driver.find_element(:name,"kywd1_exp")
@@ -14,14 +14,12 @@ def search_books(search_word)
   #日本語の本にチェックボックス
   driver.find_element(:xpath, '//*[@id="panel_0"]/div/div[3]/table/tbody/tr/td[1]/a').click
   #検索ボタンをクリック
-
   driver.find_element(:xpath, '//*[@id="chg_all_check0"]').click
   #チェックボックスにチェックを入れて全てを選択
   driver.find_element(:xpath, '//*[@id="local_list_aera"]/div[5]/div[4]/table/tbody/tr/td[3]/span/a').click
   #選択したものの情報を表示ボタンを押す
 
-  sleep(5)
-
+  sleep(7)
 
   book_title_head_xpath = '(//*[@id="bb_ttl"]/a)['
   book_title_last_xpath = ']'
@@ -29,8 +27,7 @@ def search_books(search_word)
   book_number_last_xpath = ']'
   line = "---------------------------------------------"
 
-  i = 3
-
+  i = 1
   while i <21 do
     book_title_xpath = "#{book_title_head_xpath}" +  "#{i}"  +"#{book_title_last_xpath}"
     book_number_xpath = "#{book_number_head_xpath}" + "#{i}" + "#{book_number_last_xpath}"
@@ -39,8 +36,23 @@ def search_books(search_word)
     puts driver.find_element(:xpath, "#{book_number_xpath}").text
     puts line
     i += 1
-
   end
+  #ヒットした本を出力させる
+
+  j = 1
+  while j <21 do
+    book_title_xpath = "#{book_title_head_xpath}" +  "#{j}"  +"#{book_title_last_xpath}"
+    book_number_xpath = "#{book_number_head_xpath}" + "#{j}" + "#{book_number_last_xpath}"
+    book_title = driver.find_element(:xpath, "#{book_title_xpath}").text
+    book_number = driver.find_element(:xpath, "#{book_number_xpath}").text
+    File.open("books_list.txt","a"){|f|
+      f.write("#{line}\n")
+      f.write( "タイトル : #{book_title}\n")
+      f.write("書籍番号 : #{book_number}\n")
+    }
+    j += 1
+  end
+#book_list.txtというファイルにも一応記述させる
 end
 
 
